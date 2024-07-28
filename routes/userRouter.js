aaa6d8c54900e009a5cfe3e809ab9f7b571e8b31
Reynaldo Javier <161954393+Gaudeamus013@users.noreply.github.com>
2024-07-28 12:13:17 -0400

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,10 +5,8 @@ const bookingController = require('../controllers/bookingController');
 // Endpoints para reservas
 router.post('/booking', bookingController.createBooking);
 router.get('/booking', bookingController.getBookings);
-router.get('/booking', bookingController.getBookingInfo);
 router.get('/booking/:id', bookingController.getBookingById);
 router.put('/booking/:id', bookingController.updateBooking);
 router.delete('/booking/:id', bookingController.deleteBooking);
-router.get('/booking/filter', bookingController.filterBookings);
 
 module.exports = router;
